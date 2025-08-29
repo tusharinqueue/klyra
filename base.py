36b5ad337d75566b99e8b5965f264a8de94ddbeb
Tushar Singla <tusharsingla10389@gmail.com>
2025-08-30 01:22:17 +0530

    proto1 implemented starting code

--- base.py
+++ base.py
@@ -1,6 +1,15 @@
-pickle import
+import pickle
 def create():
     l=[]
     xf=open("student.dat","wb")
     wish='y'
     while wish=='y':
+        name=input("enter the name of the student")
+        admo=int(input("enter the admisson no."))
+        Class=int(input("enter the class of student"))
+        l=[name,admo,Class]
+        pickle.dump(l,xf)
+        print(l)
+        wish=input("if you want to enter more data please enter 'y' else press 'n'.")
+    xf.close()
+create()
